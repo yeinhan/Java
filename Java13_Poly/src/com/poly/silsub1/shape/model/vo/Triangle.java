ed8b6214da7d7caa85361fb7c6be05460631fb8d
=yeinhan <=goregore8637@naver.com>
2020-04-12 23:40:47 +0900

    no message

--- Java13_Poly/src/com/poly/silsub1/shape/model/vo/Triangle.java
+++ Java13_Poly/src/com/poly/silsub1/shape/model/vo/Triangle.java
@@ -1,5 +1,45 @@
 package com.poly.silsub1.shape.model.vo;
 
-public class Triangle {
+public class Triangle implements IShape {
+
+	private double base;
+	private double height;
+	//�⺻ ������
+	public Triangle() {}
+	
+	public Triangle(double base, double height) {
+		this.base = base;
+		this.height = height;
+	}
+	@Override
+	public double area() {
+		double result = (getBase()*getHeight())/2;
+		return result;
+	}
+
+	public double getHypotenuse() {
+		double result = Math.sqrt(getBase()*getBase()+getHeight()*getHeight());
+		return result;
+	}
+	@Override
+	public double perimeter() {
+		double result = getBase()+getHeight()+getHypotenuse();
+		return result;
+	}
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
 
 }
