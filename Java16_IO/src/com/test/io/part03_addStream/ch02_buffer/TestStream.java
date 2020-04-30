4bc9c0fa919eedd6002b563ac0b8b7279fa5e40d
=yeinhan <goregore8637@naver.com>
2020-04-30 23:07:21 +0900

    끄적끄적공부...

--- Java16_IO/src/com/test/io/part03_addStream/ch02_buffer/TestStream.java
+++ Java16_IO/src/com/test/io/part03_addStream/ch02_buffer/TestStream.java
@@ -7,36 +7,69 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class TestStream {
+
+public class TestStream{
 	public void output() {
-		//new 보조스트림(new 보조스트림(new 기반(외부자원)));
-		
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter("sample2.txt"));) {
-			bw.write("안녕하세요\n");
-			bw.write("반갑습니다.\n");
-			bw.write("반갑습니다2222222.\n");
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter("sampllle2.txt"));){
 			
-		}catch(IOException e) {
+			bw.write("안녕하세요");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public void input() {
-		//trywithresource
-		
-		try (BufferedReader br = new BufferedReader(new FileReader("sample2.txt"));){
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("sampllle2.txt"));
+			String str = "";
 			
-			String tmp;
-			while((tmp=br.readLine()) !=null ) {	//null이 아닐경우에 데이터 가져옴
-				System.out.println(tmp);
+			try {
+				while((str=br.readLine())!=null) {
+					System.out.println(str);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
 	}
 }
+//public class TestStream {
+//	public void output() {
+//		//new 보조스트림(new 보조스트림(new 기반(외부자원)));
+//		
+//		try(BufferedWriter bw = new BufferedWriter(new FileWriter("sample2.txt"));) {
+//			bw.write("안녕하세요\n");
+//			bw.write("반갑습니다.\n");
+//			bw.write("반갑습니다2222222.\n");
+//			
+//		}catch(IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void input() {
+//		//trywithresource
+//		
+//		try (BufferedReader br = new BufferedReader(new FileReader("sample2.txt"));){
+//			
+//			String tmp;
+//			while((tmp=br.readLine()) !=null ) {	//null이 아닐경우에 데이터 가져옴
+//				System.out.println(tmp);
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//	}
+//}
--- Java16_IO/src/com/test/io/part04_objectStream/TestStream.java
+++ Java16_IO/src/com/test/io/part04_objectStream/TestStream.java
@@ -7,18 +7,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-public class TestStream {
+public class TestStream{
 	public void fileSave() {
-		Member[] mar = {
-				new Member("user01","pass01","한예인","han@kh.or",19,'여'),
-				new Member("user02","pass02","둘예인","han2@kh.or",20,'여'),
-				new Member("user03","pass03","삼예인","han3@kh.or",23,'여')
+		Member[] m = {
+				new Member("user01", "pass01", "한예인", "jsdf",19,'여'),
+				new Member("user01", "pass01", "한예인", "jsdf",19,'여')
 		};
 		
-		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream("member.dat"));){
-			
-			for(int i=0; i<mar.length; i++) {
-				objOut.writeObject(mar[i]);
+		try(ObjectOutputStream obOut = new ObjectOutputStream(new FileOutputStream("mem.txt"));){
+			for(int i=0; i<m.length; i++) {
+				obOut.writeObject(m[i]);
 			}
 			
 		} catch (FileNotFoundException e) {
@@ -29,26 +27,76 @@ public class TestStream {
 			e.printStackTrace();
 		}
 	}
-	
 	public void fileOpen() {
-		Member[] mar = new Member[3];
-		try (ObjectInputStream objIn = new ObjectInputStream(new FileInputStream("member.dat"));){
+		Member[] m = new Member[3];
+		try {
+			ObjectInputStream obIn = new ObjectInputStream(new FileInputStream("mem.txt"));
 			
-			for(int i=0; i<mar.length; i++) {
-				mar[i] = (Member)objIn.readObject();
+			for(int i=0; i<m.length; i++) {
+				try {
+					m[i]=(Member)obIn.readObject();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				for(Member m2:m) {
+					System.out.println(m2);
+				}
 			}
-			for(Member m: mar) {
-				System.out.println(m);
-			}
-			
-		}catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}catch (IOException e) {
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 }
+
+
+
+//public class TestStream {
+//	public void fileSave() {
+//		Member[] mar = {
+//				new Member("user01","pass01","한예인","han@kh.or",19,'여'),
+//				new Member("user02","pass02","둘예인","han2@kh.or",20,'여'),
+//				new Member("user03","pass03","삼예인","han3@kh.or",23,'여')
+//		};
+//		
+//		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream("member.dat"));){
+//			
+//			for(int i=0; i<mar.length; i++) {
+//				objOut.writeObject(mar[i]);
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public void fileOpen() {
+//		Member[] mar = new Member[3];
+//		try (ObjectInputStream objIn = new ObjectInputStream(new FileInputStream("member.dat"));){
+//			
+//			for(int i=0; i<mar.length; i++) {
+//				mar[i] = (Member)objIn.readObject();
+//			}
+//			for(Member m: mar) {
+//				System.out.println(m);
+//			}
+//			
+//		}catch(FileNotFoundException e) {
+//			e.printStackTrace();
+//		}catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+//}
